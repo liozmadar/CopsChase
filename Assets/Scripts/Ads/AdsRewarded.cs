b6c7a2a5615b128b6da56ca919b6ba36d71dcff9
liozmadar <liozmadar@gmail.com>
2023-02-05 18:07:15 +0200

    Skippable ad works good after player click "continue the game"!!

--- Assets/Scripts/Ads/AdsRewarded.cs
+++ Assets/Scripts/Ads/AdsRewarded.cs
@@ -13,6 +13,8 @@ public class AdsRewarded : MonoBehaviour, IUnityAdsListener
 #endif
     public bool testMode = true; //Leave this as True UNTIL you release your game!!!
 
+    public bool clickedOnAdForCoinsBool;
+
     void Start()
     {
         if (!Advertisement.IsReady(mySurfacingId))//this is only if the scene was reload(so there wont be 2 or more ads initialize)
@@ -26,20 +28,35 @@ public class AdsRewarded : MonoBehaviour, IUnityAdsListener
     {   // Check if UnityAds ready before calling Show method:
         if (Advertisement.IsReady(mySurfacingId)) Advertisement.Show(mySurfacingId);
         else Debug.Log("Rewarded video is not ready at the moment! Please try again later!");
+        clickedOnAdForCoinsBool = true;
     }
     public void OnUnityAdsDidFinish(string surfacingId, ShowResult showResult) // Implement IUnityAdsListener interface methods:
     {
-        if (showResult == ShowResult.Finished)
+        if (showResult == ShowResult.Finished && clickedOnAdForCoinsBool)
         {
             print("The Ad finished!!!");
             int RewardedAdCoins = ScoreSystem.instance.totalScorePoints += 2500;
             PlayerPrefs.SetInt("totalScorePoints", RewardedAdCoins);
             CanvasManager.instance.adButton.SetActive(false);
             CanvasManager.instance.adButtonBool = false;
+
+            clickedOnAdForCoinsBool = false;
+        }
+        else if (showResult == ShowResult.Finished)
+        {
+            //player click on continue play, and then he close the ad(this and the skip are the same)
+            Debug.Log("Close the inter ad");
+
+            PlayerMovment.instance.AfterClickedContinue();
+            CanvasManager.instance.oneTimeContinue.SetActive(false);
         }
         else if (showResult == ShowResult.Skipped)
         {
+            //player click on continue play, and then he skip the ad(this and the Close are the same)
             print("The Ad was SKIPPED you Cheater...");
+
+            PlayerMovment.instance.AfterClickedContinue();
+            CanvasManager.instance.oneTimeContinue.SetActive(false);
         }
         else if (showResult == ShowResult.Failed) print("The Ad was interrupted or Failed.");
     }
